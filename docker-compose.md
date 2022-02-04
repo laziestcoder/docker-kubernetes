85d8ff192cba93005e344123138ca8cf5839499e
towfiq <towfiq.106@gmail.com>
2022-02-05 02:16:06 +0600

    docker-compose and its usage

--- docker-compose.md
+++ docker-compose.md
@@ -1,4 +1,6 @@
 #### Commands:
 * docker-compose up // docker run myimage
 * docker-compose up --build // docker build . + docker run myimage
-* 
+* docker-compose up -d // -d for running container in background
+* docker-compose down // for stopping running containers
+* docker-compose ps // this command works on that directtory where docker-compose file exists 
--- nodeJsAppV1.1/index.js
+++ nodeJsAppV1.1/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const redis = require('redis');
+const process =  require('process');
 
 const app = express();
 const client = redis.createClient({
@@ -10,7 +11,7 @@ client.set('visits', 0);
 
 
 app.get('/', (req, res) => {
-
+	// process.exit(0); // non-zero means NOT OK. 0 means OK
 	client.get('visits', (err,visits) => {
 		res.send('Number of visits is ' + visits);
 		client.set('visits', parseInt(visits) + 1);
